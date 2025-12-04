057e029d4ef1f4b00e0af2f03223a3a316bcffd9
Bui-ngoc <ngocbp.ptit@gmail.com>
2025-12-04 23:16:08 +0600

    update chucVuApi and phongBanApi

--- src/api/chucVuApi.js
+++ src/api/chucVuApi.js
@@ -1,7 +1,10 @@
 import axiosClient from './axiosClient';
 
 const chucVuApi = {
-  getAll: () => axiosClient.get('/chucvu'),
+  getAll: async () => {
+    const res = await axiosClient.get('/chucvu');
+    return res?.data?.data ?? res?.data ?? [];
+  },
   get: (id) => axiosClient.get(`/chucvu/${id}`),
   create: (data) => axiosClient.post('/chucvu', data),
   update: (id, data) => axiosClient.put(`/chucvu/${id}`, data),
--- src/api/phongBanApi.js
+++ src/api/phongBanApi.js
@@ -1,8 +1,16 @@
 import axiosClient from './axiosClient';
 
 const phongBanApi = {
-  getAll: () => axiosClient.get('/phongban'),
-  get: (id) => axiosClient.get(`/phongban/${id}`),
+  getAll: async () => {
+    const res = await axiosClient.get('/phongban');
+    return res?.data?.data ?? res?.data ?? [];
+  },
+  getTongLuong: (thang, nam) => {
+    return axiosClient.get('/phongban/summary', { params: { thang, nam } });
+  },
+  get: (id, thang, nam) => {
+    return axiosClient.get(`/phongban/${id}`, { params: { thang, nam } });
+  },
   create: (data) => axiosClient.post('/phongban', data),
   update: (id, data) => axiosClient.put(`/phongban/${id}`, data),
   delete: (id) => axiosClient.delete(`/phongban/${id}`)
